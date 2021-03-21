0233675873e734cca4152079a4b5f10b03e40f85
Shandy Brown <github@ezide.com>
2021-03-20 18:22:35 -0700

    profile defaults

--- src/storage.js
+++ src/storage.js
@@ -6,12 +6,12 @@ var storage = {
 
   setPreference: function(key, val) {
     return localStorage.setItem(key, val)
-  }
+  },
 
   setDefaultPreferences: function() {
     defaults = {
       'profile_color': '#ffffff',
-      'profile_name': base32.short_id(),
+      'profile_name': 'user_' + base32.chars(3),
     }
     for (key in defaults) {
       if (storage.getPreference(key) === null) {
